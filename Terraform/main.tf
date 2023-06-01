c0741255dac021544b1c7dab2383f66fc8a42359
bacalu93 <115462674+bacalu93@users.noreply.github.com>
2023-06-01 20:40:21 +0200

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -24,6 +24,12 @@ resource "aws_security_group" "nginx" {
   }
 
   ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "ssh"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
     from_port   = 9000
     to_port     = 9000
     protocol    = "tcp"
